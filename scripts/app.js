82ee0829b5dc0fc54fa641b9c3bdc10ed725f138
juanjo medina <juantltc99@gmail.com>
2024-09-12 20:23:42 -0300

    correccion de errores

--- scripts/app.js
+++ scripts/app.js
@@ -12,15 +12,19 @@ const addPersona = (e) => {
     const name = document.getElementById('name').value;
     const lastName = document.getElementById('lastName').value;
     const fileNumber = Math.floor(Math.random() * 90000) + 10000;
-    console.log(lastName);
 
     controlPersona(name,lastName,fileNumber);
-    
+
 };
 
+
 const controlPersona = (name,lastName,fileNumber) =>{
+    
     if (typeof name==='string' && typeof lastName==='string' && isNaN(name) && isNaN(lastName)) {
-        studentsLS.push(name,lastName,fileNumber) ;
+        
+        students.push({name,lastName,fileNumber}) ;
+        console.log(students);
+        
         localStorage.setItem("students", JSON.stringify(students));
         swal("Listo","Datos validados",'success');
     } else {
